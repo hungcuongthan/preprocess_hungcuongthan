402f271609cf44360a9711e78cd3b7e81159b56a
tthc1308 <67635895+hungcuongthan@users.noreply.github.com>
2021-07-26 19:22:57 +0200

    update all

--- Preprocessing Package/setup.py
+++ Preprocessing Package/setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open('README.md','r') as file:
+with open('README.md', 'r') as file:
     long_description = file.read()
 
 setuptools.setup(
@@ -12,9 +12,8 @@ setuptools.setup(
     long_description_content_type = 'text/markdown',
     packages = setuptools.find_packages(),
     classifiers = [
-        'Programming Language :: Python :: 3',
-        'License :: OSI Aproved :: MIT License',
-        'Operating System :: OS Independent'
-    ],
-    python_requires = '>= 3.5'    
-)
+    'Programming Language :: Python :: 3',
+    'License :: OSI Aproved :: MIT License',
+    "Operating System :: OS Independent"],
+    python_requires = '>=3.5'
+    )
